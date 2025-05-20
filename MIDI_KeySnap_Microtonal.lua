86a94299a5b55da7686a7193a8533c362774ea3f
Christopher Josephs / Digibitsy <xiss23@gmail.com>
2025-05-20 20:03:19 +0100

    example scale file

--- MIDI_KeySnap_Microtonal.lua
+++ MIDI_KeySnap_Microtonal.lua
@@ -1,4 +1,4 @@
-/*******************************************************************************
+v/*******************************************************************************
 *  Copyright 2007 - 2011, Philip S. Considine                                  *
 *  This program is free software: you can redistribute it and/or modify        *
 *  it under the terms of the GNU General Public License as published by        *
@@ -11,10 +11,10 @@
 *  GNU General Public License (http://www.gnu.org/licenses/)for more details.  *
 *******************************************************************************/
 
-desc: MIDI Snap To Key
-desc: MIDI Snap To Key [IXix]
+desc: MIDI Snap To Key Microtonal
+desc: MIDI Snap To Key Microtonal [original by IXix - microtonal support added by crimzonclotta]
 //tags: MIDI processing mapping
-//author: IXix
+//author: IXix, crimzonclotta
 
 slider1:0<0,15,1{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}>Input Channel
 
@@ -25,10 +25,10 @@ slider3:127<0,127,1>Note Max
 //slider2:0<0,127,1{C0,C#0,D0,D#0,E0,F0,F#0,G0,G#0,A0,A#0,B0,C1,C#1,D1,D#1,E1,F1,F#1,G1,G#1,A1,A#1,B1,C2,C#2,D2,D#2,E2,F2,F#2,G2,G#2,A2,A#2,B2,C3,C#3,D3,D#3,E3,F3,F#3,G3,G#3,A3,A#3,B3,C4,C#4,D4,D#4,E4,F4,F#4,G4,G#4,A4,A#4,B4,C5,C#5,D5,D#5,E5,F5,F#5,G5,G#5,A5,A#5,B5,C6,C#6,D6,D#6,E6,F6,F#6,G6,G#6,A6,A#6,B6,C7,C#7,D7,D#7,E7,F7,F#7,G7,G#7,A7,A#7,B7,C8,C#8,D8,D#8,E8,F8,F#8,G8,G#8,A8,A#8,B8,C9,C#9,D9,D#9,E9,F9,F#9,G9,G#9,A9,A#9,B9,C10,C#10,D10,D#10,E10,F10,F#10,G10}>Note Min
 //slider3:127<0,127,1{C0,C#0,D0,D#0,E0,F0,F#0,G0,G#0,A0,A#0,B0,C1,C#1,D1,D#1,E1,F1,F#1,G1,G#1,A1,A#1,B1,C2,C#2,D2,D#2,E2,F2,F#2,G2,G#2,A2,A#2,B2,C3,C#3,D3,D#3,E3,F3,F#3,G3,G#3,A3,A#3,B3,C4,C#4,D4,D#4,E4,F4,F#4,G4,G#4,A4,A#4,B4,C5,C#5,D5,D#5,E5,F5,F#5,G5,G#5,A5,A#5,B5,C6,C#6,D6,D#6,E6,F6,F#6,G6,G#6,A6,A#6,B6,C7,C#7,D7,D#7,E7,F7,F#7,G7,G#7,A7,A#7,B7,C8,C#8,D8,D#8,E8,F8,F#8,G8,G#8,A8,A#8,B8,C9,C#9,D9,D#9,E9,F9,F#9,G9,G#9,A9,A#9,B9,C10,C#10,D10,D#10,E10,F10,F#10,G10}>Note Max
 slider4:root_midi_note=60<0,127,1>Root MIDI Note (0-127)
-slider4:0<0,11,1{C,C#,D,Eb,E,F,F#,G,G#,A,Bb,B}>Root Note
-slider5:/ix_scales:none:Scale File
+slider5:/tuning/midi_keysnap_microtonal/scales:none:Scale File
 slider6:1<0,1,1{Block,Remap}>Mode
 slider7:1<0,1,1{Off,On}>On/Off
+slider8:/tuning/midi_keysnap_microtonal/tunings:none:Tuning File
 
 in_pin:none
 out_pin:none
@@ -193,3 +193,4 @@ while
     1; // Force loop to continue until all messages have been processed
   );
 );
+
